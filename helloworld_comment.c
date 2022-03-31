d59231052bb46b68b138f620264b59d1e18c6531
jullia021228@gmail.com <jullia021228@gmail.com>
2022-03-31 22:10:48 +0200

    upload

---
+++ helloworld_comment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  // abcd
+  printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+  /* hi
+  */
+  printf("Learning the C programming language is an exciting adventure!\n");
+}
