c66ef63660e2dd88751b36c87ff51db5b48a65ef
jullia021228@gmail.com <jullia021228@gmail.com>
2022-04-02 11:29:13 +0200

    this is for 01th April 2022

---
+++ variables_naming_standards.c
@@ -0,0 +1,25 @@
+/* #include<stdio.h>
+
+int main() {
+  
+  int number!;
+  int test value;
+  int 1example;
+  float int;
+  printf("Hello World!\n");
+
+} 
+this version causes errors  */
+
+//proper version//
+#include<stdio.h>
+
+int main() {
+  
+  int number;
+  int test_value;
+  int example;
+  //float int;
+  printf("Hello World!\n");
+
+}
