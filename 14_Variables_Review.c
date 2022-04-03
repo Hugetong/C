1721ad54d80e8b95aaee44a9213bad201c6cccae
jullia021228@gmail.com <jullia021228@gmail.com>
2022-04-03 16:51:46 +0200

    3th april 2022

---
+++ 14_Variables_Review.c
@@ -0,0 +1,24 @@
+//Free Coding//
+
+#include <stdio.h>
+
+int main() {
+  
+  int variableInt = 3;
+  char variableChar = 'a';
+  double variableDouble = 1.44;
+  float variableFloat = 2.44;
+  // Feel free to play around with what you have learned about variables here. You can also create new ones if you like, but right now only the ones created for you are set to be displayed.
+variableInt = 4;
+variableChar = 'b';
+variableDouble = (double)variableFloat;
+
+
+
+
+  // Output logic
+  printf("display an int here: %d\n", variableInt);
+  printf("display a char here: %c\n", variableChar);
+  printf("display a double with 2 decimal places here: %.2f\n", variableDouble);
+
+}
