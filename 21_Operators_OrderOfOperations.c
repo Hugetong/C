8dbb6f92f3170e469cb263d227bc1075233c1e25
jullia021228@gmail.com <jullia021228@gmail.com>
2022-04-05 11:37:33 +0200

    5th april 2022

---
+++ 21_Operators_OrderOfOperations.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  
+  int x;
+  int y;
+
+  x = (2 + 3) * 5;          //일반 수학에서처럼 ()로 먼저 계산하고 싶은 거 표시!
+  y = 2 / (4 + 6);
+
+  printf("x is: %d\n", x);
+  printf("y is: %d\n", y);
+}
