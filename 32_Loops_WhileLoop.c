d8109350cdfc18d4fdf8254a1e9fa4eace76731b
hugetung <jullia021228@gmail.com>
2022-04-07 08:58:28 +0200

    7th april 2022

---
+++ 32_Loops_WhileLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+  int i = 9;
+  int square = 81;
+
+  // Write a while loop here:
+  while(i>=0 && i<=9) {
+    printf("%d\t%d\n", i, i*i);
+    i--;
+  }
+
+}
